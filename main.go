dcba46d968c46a2e7d799c19d2ff41a64364c475
William Perron <hey@wperron.io>
2021-11-12 08:43:54 -0500

    spawn 10 syscalls in goroutines

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"syscall"
 	"time"
 )
@@ -9,12 +10,20 @@ import (
 func main() {
 	fmt.Println(time.Now())
 
-	if err := syscall.Nanosleep(&syscall.Timespec{
-		Sec:  1,
-		Nsec: 0,
-	}, nil); err != nil {
-		panic(err)
+	wg := new(sync.WaitGroup)
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			if err := syscall.Nanosleep(&syscall.Timespec{
+				Sec:  1,
+				Nsec: 0,
+			}, nil); err != nil {
+				panic(err)
+			}
+			wg.Done()
+		}(i)
 	}
 
+	wg.Wait()
 	fmt.Println(time.Now())
 }
